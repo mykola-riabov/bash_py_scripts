14fbc96f0403fc961887eb19b8d7745804f41239
mriabov <mykola.riabov@gmail.com>
2023-10-18 18:07:48 +0200

    create dynamic

---
+++ py/ai/openai/chat_dynamic/README.md
@@ -0,0 +1,34 @@
+
+---
+
+# GPT-3.5 Chat Application
+
+This repository contains a simple interactive chat application that allows users to have a conversation with OpenAI's GPT-3.5 model. The application is built using Python and the OpenAI GPT-3.5 API.
+
+## Features
+
+- **Chat with GPT-3.5**: Users can engage in a chat-like conversation with the GPT-3.5 model by entering text-based questions and receiving AI-generated responses.
+
+- **Dynamic Conversation Context**: The application maintains a dynamic conversation context, incorporating user input and AI-generated responses to create a more interactive and context-aware conversation.
+
+- **Exit Option**: Users can choose to exit the conversation at any point by typing 'exit'.
+
+## Usage
+
+1. Clone this repository to your local machine.
+
+2. Set up an OpenAI API key and replace `<YOUR_API_KEY>` in the code with your actual API key.
+
+3. Run the Python script to start the chat application.
+
+4. Enter your questions or responses during the conversation. To exit the conversation, simply type 'exit'.
+
+## Dependencies
+
+- [OpenAI Python](https://github.com/openai/openai-python): Official Python client for the OpenAI GPT-3.5 API.
+
+## Getting Started
+
+To get started, follow the instructions in the [Usage](#usage) section above.
+
+---
---
+++ py/ai/openai/chat_dynamic/chat_gpt_dynamic.py
@@ -0,0 +1,49 @@
+import openai
+
+# Replace YOUR_API_KEY with your OpenAI API key
+api_key = "YOUR_API_KEY"
+
+# Install the API key
+openai.api_key = api_key
+
+
+def chat_with_gpt(question, context, max_tokens=1000, temperature=1):
+    # Concatenate the question and context to create a chat-like interaction
+    prompt = f"You: {question}\nAI: {context}\nYou:"
+
+    # We are forming a query to GPT-3.5 for generating a chat-like continuation
+    response = openai.Completion.create(
+        engine="gpt-3.5-turbo-instruct",
+        prompt=prompt,
+        max_tokens=max_tokens,  # Maximum number of tokens in the response
+        temperature=temperature,  # Temperature parameter for response diversity
+        stop=None  # Stop condition for ending the response
+    )
+
+    # Extract the AI's reply from the GPT-3.5 response
+    reply = response.choices[0].text.strip()
+    return reply
+
+
+if __name__ == "__main__":
+    print("You are chatting with a GPT-3.5 model. Type 'exit' to end the conversation.")
+
+    # Initialize context as an empty string
+    context = ""
+
+    while True:
+        user_input = input("You: ")
+
+        # Allow the user to exit the chat
+        if user_input.lower() == 'exit':
+            break
+
+        # Incorporate user input into the conversation context
+        context += f"You: {user_input}\n"
+
+        # Get AI's reply
+        gpt_reply = chat_with_gpt(user_input, context, max_tokens=1000, temperature=1)
+        print("AI:", gpt_reply)
+
+        # Update the context with AI's reply
+        context += f"AI: {gpt_reply}\n"
