bd434288739ddf1fefbd82032644e7f1594a3313
mriabov <mykola.riabov@gmail.com>
2023-10-18 17:33:15 +0200

    openai chat

---
+++ py/ai/openai/chat/README.md
@@ -0,0 +1,26 @@
+
+---
+
+# GPT-3.5-powered Question Answering Program
+
+This Python program utilizes the power of OpenAI's GPT-3.5 Turbo to generate answers to questions provided by the user. The program prompts the user for a question and a context and then generates an answer in the specified target language using GPT-3.5 Turbo.
+
+## Usage
+
+1. Clone this repository.
+
+2. Install the necessary Python packages using pip:
+
+   ```bash
+   pip install openai
+   ```
+
+3. Replace `YOUR_API_KEY` with your OpenAI API key.
+
+4. Run the program and input the question and context when prompted.
+
+## How it Works
+
+The program leverages GPT-3.5 Turbo, a highly advanced language generation model by OpenAI. It forms a query to GPT-3.5 Turbo by providing a question and context as input. The GPT-3.5 Turbo model generates a response to the question based on the input provided, and the program extracts and displays the answer.
+
+---
---
+++ py/ai/openai/chat/chat_gpt.py
@@ -0,0 +1,32 @@
+import openai
+
+# Replace YOUR_API_KEY with your OpenAI API key
+api_key = "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"
+
+# Install the API key
+openai.api_key = api_key
+
+def generate_answer(question, context, target_language="en", max_tokens=1000, temperature=1):
+    # We are forming a query to GPT-3.5 for generating an answer to the question
+    response = openai.Completion.create(
+        engine="gpt-3.5-turbo-instruct",
+        prompt=f"Question: {question}\nContext: {context}\nAnswer:",
+        max_tokens=max_tokens,  # Maximum number of tokens in the response
+        temperature=temperature,  # temperature parameter for response diversity
+        stop=None  # Stop condition for ending the response
+    )
+
+    # We get the answer from the GPT-3.5 response
+    answer = response.choices[0].text.strip()
+    return answer
+
+# Prompting the user for a question
+question = input("Enter your question: ")
+
+# Prompting the user for context
+context = input("Enter the context: ")
+
+# Generating the answer to the question with a high value of max_tokens
+russian_answer = generate_answer(question, context, target_language="ru", max_tokens=1000, temperature=1)
+print("Question:", question)
+print("Answer:", russian_answer)
