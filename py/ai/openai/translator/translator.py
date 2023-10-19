a20de8301e3d4e3239b74a891f6bf7feb997660a
mriabov <mykola.riabov@gmail.com>
2023-10-19 18:50:01 +0200

    fix translator

--- py/ai/openai/translator/translator.py
+++ py/ai/openai/translator/translator.py
@@ -1,17 +1,27 @@
+#!/usr/bin/python3
+
 import openai
 
 # Replace YOUR_API_KEY with your API key from OpenAI
-api_key = "<YOUR_API_KEY>"
+api_key = "YOUR_API_KEY"
 
 # Set the API key
 openai.api_key = api_key
 
 
-def translate_text(text, target_language="en"):
+def translate_text(text, target_language="en", source_language="en"):
+    # Determine the source and target languages
+    if source_language == "en" and target_language == "ru":
+        prompt = f"Translate the following English text to Russian: '{text}'"
+    elif source_language == "ru" and target_language == "en":
+        prompt = f"Translate the following Russian text to English: '{text}'"
+    else:
+        return "Invalid language parameters"
+
     # Form a request to GPT-3.5 for text translation
     response = openai.Completion.create(
         engine="text-davinci-002",
-        prompt=f"Translate the following English text to {target_language}: '{text}'",
+        prompt=prompt,
         max_tokens=1000  # Maximum number of tokens in the response
     )
 
@@ -20,10 +30,23 @@ def translate_text(text, target_language="en"):
     return translation
 
 
-# ЗRequest text for translation from the user
-english_text = input("Enter the text for translation to Russian: ")
+while True:
+    # Request the source language from the user
+    source_language = input("Choose the source language (en/ru): ").lower()
+    if source_language not in ["en", "ru"]:
+        print("Invalid language. Please choose 'en' or 'ru'.")
+        continue
 
-# Translate the text to russian
-russian_translation = translate_text(english_text, target_language="ru")
-print("English text:", english_text)
-print("Russian translation:", russian_translation)
+    user_input = input(f"Enter the text for translation to {source_language} (type 'exit' to quit): ")
+
+    if user_input.lower() == 'exit':
+        print("Exiting the program.")
+        break
+
+    # Determine the target language
+    target_language = "ru" if source_language == "en" else "en"
+
+    # Translate the text to the target language
+    translation = translate_text(user_input, target_language, source_language)
+    print(f"{source_language.capitalize()} text:", user_input)
+    print(f"{target_language.capitalize()} translation:", translation)
